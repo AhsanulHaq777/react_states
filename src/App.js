ac823d14d3360d83cae3ce02b71658a7ec8f6caa
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-06-11 21:44:13 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counting from './Counter.js'
 
 function App() {
+  
+  let [count,setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className={`Box ${isMorning ? 'Day' : 'Night'}`}>
+
+          <h3>It is {isMorning ? 'Morning' : 'Night'}</h3>
+
+          <Counting countvalue={count}/>
+          <button onClick={()=> setCount(count + 1)}>Click to count</button>
+
+          <button onClick={()=>setMorning(!isMorning)}>Update Day</button>
+        </div>
+          <br/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Counting(props){
+return(
+    <div> 
+        <h3>Counter value is {props.countvalue}</h3>
+        
+    </div>
+)
+}
+
+export default Counting;
